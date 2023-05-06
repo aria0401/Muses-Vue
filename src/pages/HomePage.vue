246b47dcb5542c346ae0c4e56ce6dddae0656b81
Ariadna Victorero <aria@html24.net>
2023-05-06 13:52:08 +0200

    fetch img from wp

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,7 +2,8 @@
     <div class="home-page pt-lg-5">
         <div class="row justify-content-around">
             <div class="col-left col-md-6 col-lg-5 mb-4">
-                <img :src="`${publicPath}pablo-picasso.jpg`"/>
+                <img src="http://ariadna.dk/mios/WP/wp-content/uploads/2023/05/pablo-picasso.jpg">
+                <!-- <img :src="`${publicPath}pablo-picasso.jpg`"/> -->
                 <!-- <img :src="picasso" /> -->
                 <figcaption class="mt-3">Foto: George Stroud/Getty Images</figcaption>
             </div>
